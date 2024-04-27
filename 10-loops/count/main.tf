2ebde0ef09b93ef2c8132b23916b92c65c27bdf4
sandeep <munagalasandeepreddy@gamail.com>
2024-04-27 22:15:38 +0200

    learn terraform

---
+++ 10-loops/count/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "web" {
+  count = length(var.name)
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.name[count.index] # it gives same value as element(var.name , count.index)
+  }
+}
+
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+variable "name" {
+  default = [frontend ,mongodb,catalogue,redis,user,cart,mysql,shipping, rabbitmq, payment,dispatch]
+}
