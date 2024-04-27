fdc2c914ec01f07b7164e59f9f38ee5ef11ac762
sandeep <munagalasandeepreddy@gamail.com>
2024-04-27 22:27:07 +0200

    learn terraform

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -7,3 +7,12 @@ output "class" {
 output "upper" {
   value = upper(var.class)
 }
+
+
+variable "subjects" {
+  default = ["research methodology","secure software"]
+}
+
+output "subjects" {
+  value = element(var.subjects, count.index )
+}
