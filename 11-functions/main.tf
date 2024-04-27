7b5b8b99f960e8f8a75891e30ecdae3799711523
sandeep <munagalasandeepreddy@gamail.com>
2024-04-27 22:31:38 +0200

    learn terraform

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -1,18 +1,54 @@
 variable "class" {
-  default = "hello"
+  default = "devops"
 }
+
+## Print the same in capital leetters
 output "class" {
-  value = var.class
-}
-output "upper" {
   value = upper(var.class)
 }
 
-
-variable "subjects" {
-  default = ["research methodology","secure software"]
+variable "fruits" {
+  default = ["apple", "banana", "orange"]
 }
 
-output "subjects" {
-  value = var.subjects[0]
+output "fruit_count" {
+  value = length(var.fruits)
+}
+
+variable "classes" {
+  default = {
+    devops = {
+      name   = "devops"
+      topics = ["jenkins", "docker"]
+    }
+    aws = {
+      name = "aws"
+    }
+  }
+}
+
+output "devops_topics" {
+  value = var.classes["devops"]["topics"]
+}
+
+//output "aws_topics" {
+//  value = lookup(lookup(var.classes, "aws", null), "topics", "No Topics So far")
+//}
+
+//output "aws_topics" {
+//  value = lookup(lookup(var.classes, "aws", null), "topics", null) == null ? "No AWS Training So far" : lookup(lookup(var.classes, "aws", null), "topics", null)
+//}
+
+output "fruit_4" {
+  value = element(var.fruits, 4)
+}
+
+variable "a" {
+  default = 100
+}
+
+output "a" {
+  value = var.a > 50  ? "Surplus Quantity" : "Less Quantity"
+
+  ] ` we12
 }
