c4c68787ac149522cfd16b35ba7e45138a3f01fb
sandeep <munagalasandeepreddy@gamail.com>
2024-04-27 09:46:43 +0200

    learn terraform

--- 03-variables/output.tf
+++ 03-variables/output.tf
@@ -34,3 +34,54 @@ output "sample-ext" {
 # 3. Map
 
 ## Plain
+variable "course" {
+  default = "DevOps Training"
+}
+
+# List
+variable "courses" {
+  default = [
+    "DevOps",
+    "AWS",
+    "Python"
+  ]
+}
+
+## Map
+variable "course_details" {
+  default = {
+    DevOps = {
+      name     = "DevOps"
+      timing   = "10am"
+      duration = 90
+    }
+    AWS = {
+      name     = "AWS"
+      timing   = "11am"
+      duration = 30
+    }
+  }
+}
+
+output "course" {
+  value = var.course
+}
+
+output "courses" {
+  value = var.courses[2]
+}
+
+output "course_details" {
+  value = var.course_details["DevOps"]
+}
+
+variable "env" {}
+
+output "env" {
+  value = var.env
+}
+
+variable "url" {}
+output "url" {
+  value = var.url
+}
